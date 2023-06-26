33085f23e700e7da7aae3cbbb24ec95c0e248fa7
s1093977 <4376708+TomScherndl@users.noreply.github.com>
2023-06-26 18:30:42 +0200

    again a rebase... never mess with git again...

--- Assignment/A4/dashboard.py
+++ Assignment/A4/dashboard.py
@@ -11,8 +11,12 @@ def sidebar():
     '''
     Print information in sidebar - mostly used for filtering
     '''
+    yearrange = (2014, 2015, 2016, 2017,2018, 2019)
+    fueltype = ("wind", "solar", "gas", "oil", "nuclear")
     with st.sidebar: 
-        st.write("## Select Data and Filter \n This is info in the sidebar! Later we will include filter options!")
+        st.write("## Select Data and Filter \n This is info in the sidebar! Later we will include filter options! This is not yet changing anything in the main window!")
+        SelectYear = st.select_slider("Choose year!", value = 2019, options = yearrange)
+        SelectFuelType = st.selectbox("Choose type of fuel:", fueltype)
 
 
 def map(): 
@@ -33,3 +37,10 @@ def dashboard(data = None):
     This function is for printing the relevant statistics and graphs below the map
     '''
     st.markdown("## Statistics and Data \n This is text for the dashboard (=below the map) from the respective function. ")
+
+    dataPS = st.session_state["dataPS"]
+
+    # add metrics
+    m1, m2, m3 = st.columns(3)
+    m1.metric("Number of Powerplants", dataPS["gppd_idnr"].count())
+    m2.metric("Overall Capacity in MW", round(dataPS["capacity_mw"].sum(),0))
--- Assignment/A4/preprocess.py
+++ Assignment/A4/preprocess.py
@@ -13,5 +13,6 @@ def getPowerStationData(file = "FinalProject\PLUS_softwaredev_2023_PowerGenerati
 
     data = pd.read_csv(filepath_or_buffer=file)
     # put it into session state for later use - also in other modules!
+    data = data[data["country_long"] == "Australia"]
     st.session_state["dataPS"] = data
     return data
